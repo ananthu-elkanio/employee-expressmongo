b4d383b174de99be310bdcf4c44e3c5bccaf43eb
Avas <34262142+ananthu-va@users.noreply.github.com>
2023-02-03 10:22:46 +0530

    enable cors.

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL
 const app = express();
+var cors = require('cors')
 
-
+app.use(cors())
 
 mongoose.connect(mongoString);
 const database = mongoose.connection
